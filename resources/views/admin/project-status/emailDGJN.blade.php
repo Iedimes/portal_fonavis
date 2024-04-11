7bf392d410635ae642c35c8ee216509d318d5e8e
MUVH\osemidei <osemidei@gmail.com>
2024-04-11 08:03:40 -0400

    formato del mail que se remite a DGJN

---
+++ resources/views/admin/project-status/emailDGJN.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Correo Seleccion Preliminar</title>
+</head>
+<body>
+    <p style="text-align: justify">La <strong>DGFO</strong> solicita la revisión preliminar del proyecto consistente en análisis de las condiciones legales del inmueble del
+     proyecto <strong>"{{ $proyecto }} - {{ $id }}"</strong>, postulado por el SAT <strong>"{{ $satnombre['NucNomSat'] }}"</strong>.</p>
+</body>
+</html>
