93c388d2830b9f1293333ebb9d09c7ce2263cedf
MUVH\osemidei <osemidei@gmail.com>
2025-04-14 13:49:53 -0300

    Se le habilita a juridico para que puedan seguir viendo en todos los estados que corresponde a juridico el historial del proyecto

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -237,6 +237,15 @@
                                                     <div class="p-1" v-if="item.get_estado && item.get_estado.stage_id === 2">
                                                         <a class="btn btn-sm btn-warning" :href="item.resource_url + '/showDGJN'" title="{{ trans('brackets/admin-ui::admin.btn.show') }}" role="button"><i class="fa fa-search"></i></a>
                                                     </div>
+                                                    <div class="p-1" v-if="item.get_estado && item.get_estado.stage_id === 3">
+                                                        <a class="btn btn-sm btn-warning" :href="item.resource_url + '/showDGJN'" title="{{ trans('brackets/admin-ui::admin.btn.show') }}" role="button"><i class="fa fa-search"></i></a>
+                                                    </div>
+                                                    <div class="p-1" v-if="item.get_estado && item.get_estado.stage_id === 4">
+                                                        <a class="btn btn-sm btn-warning" :href="item.resource_url + '/showDGJN'" title="{{ trans('brackets/admin-ui::admin.btn.show') }}" role="button"><i class="fa fa-search"></i></a>
+                                                    </div>
+                                                    <div class="p-1" v-if="item.get_estado && item.get_estado.stage_id === 6">
+                                                        <a class="btn btn-sm btn-warning" :href="item.resource_url + '/showDGJN'" title="{{ trans('brackets/admin-ui::admin.btn.show') }}" role="button"><i class="fa fa-search"></i></a>
+                                                    </div>
                                                 @endif
 
                                                 @if (Auth::user()->rol_app->dependency_id == 1)
