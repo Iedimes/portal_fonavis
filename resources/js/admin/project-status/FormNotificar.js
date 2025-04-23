5ffe0922d0fbfdabe92b082875e4ff7b353d698e
MUVH\osemidei <osemidei@gmail.com>
2025-04-23 11:35:56 -0300

    correccion para que mentenga el estado en que se encuentra en la notificacion

--- resources/js/admin/project-status/FormNotificar.js
+++ resources/js/admin/project-status/FormNotificar.js
@@ -2,12 +2,12 @@ import AppForm from '../app-components/Form/AppForm';
 
 Vue.component('project-status-form-notificar', {
     mixins: [AppForm],
-    props: ["project","user"],
+    props: ["project","user", 'estado'],
     data: function() {
         return {
             form: {
                 project_id:  this.project,
-                stage_id:  2,
+                stage_id:  this.estado,
                 user_id:  this.user,
                 record:  '' ,
 
