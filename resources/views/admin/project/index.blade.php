ceceab6d0a97095194eccf9dcc793cca21081a45
Iedimes <osemidei@gmail.com>
2025-07-19 12:54:50 -0300

    Cambios para que DIGH pueda ver los documentos levantados por SAT y pueda cambiar el estado

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -326,7 +326,7 @@
 
         {{-- DEPENDENCY 4 --}}
         @if (Auth::user()->rol_app->dependency_id == 4)
-            <div class="p-1" v-if="item.get_estado && item.get_estado.stage_id === 12">
+            <div class="p-1" v-if="item.get_estado && item.get_estado.stage_id === 11">
                 <a class="btn btn-sm btn-warning" :href="item.resource_url + '/showDIGH'" title="{{ trans('brackets/admin-ui::admin.btn.show') }}" role="button">
                     <i class="fa fa-search"></i>
                 </a>
