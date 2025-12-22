f9b97f9aad7c5f20ade304ca74d313236540d468
osemidei <osemidei@gmail.com>
2025-12-22 11:14:06 -0300

    Ocultar boton de crear proyectos cuando no encuentra ningun proyecto en bandeja

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -636,8 +636,8 @@
                                 <i class="icon-magnifier"></i>
                                 <h3>{{ trans('brackets/admin-ui::admin.index.no_items') }}</h3>
                                 <p>{{ trans('brackets/admin-ui::admin.index.try_changing_items') }}</p>
-                                <a class="btn btn-primary " href="{{ url('admin/projects/create') }}" role="button"><i
-                                        class="fa fa-plus"></i>&nbsp; {{ trans('admin.project.actions.create') }}</a>
+                                {{-- <a class="btn btn-primary " href="{{ url('admin/projects/create') }}" role="button"><i
+                                        class="fa fa-plus"></i>&nbsp; {{ trans('admin.project.actions.create') }}</a> --}}
                             </div>
                         </div>
                     </div>
